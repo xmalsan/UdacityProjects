cd19916f00a52ba826fc2892adf8dd07f542e756
Mallika Sanyal <mallika.sanyal@amdocs.com>
2019-12-01 10:26:05 +0530

    Udagram as microservice

---
+++ UdagramAsMicroservice/udacity-c2-restapi-user/src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "udagram01",
+    "password": "udagram01",
+    "database": "udagram",
+    "host": "udagram.cfkhn44eii2q.ap-south-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "ap-south-1b",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-xmalsan-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
